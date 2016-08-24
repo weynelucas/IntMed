cc1a2d44b8a9b2c1bffa0300769aab9611d55d18
Lucas Weyne <weynelucas@gmail.com>
2016-08-24 08:13:33 -0300

    query_service refac

--- TreatCare/TreatCare/utils/query_service.py
+++ TreatCare/TreatCare/utils/query_service.py
@@ -2,23 +2,18 @@ from django.apps import apps
 from django.db.models.sql.constants import QUERY_TERMS
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 
-def perform_query(app_label, model_name, request):
+def perform_query(app_label, model_name, params):
     """ Performs a query in a model given the request
         parameters
         Args:
             app_label: app containing the model to perform the query
             model_name: name of model to perform the query
-            request: parameters of query
+            params: request parameters
         Return:
             List of objects (queryset)
     """
     Model = apps.get_model(app_label=app_label, model_name=model_name)
     fields = [field.name for field in Model._meta.fields]
-
-    if request.method == 'POST':
-        params = request.POST
-    else:
-        params = request.GET
 
     sort = params.get('sort', '')
     order = params.get('order', 'asc')
@@ -36,18 +31,18 @@ def perform_query(app_label, model_name, request):
     return queryset
 
 
-def paginate_list(instance_list, request, instances_per_page=20):
+def paginate_list(instance_list, params, instances_per_page=20):
     """ Paginate a list of objects
         Args:
             instance_list: list of objects to paginate
-            request: HTTP request
+            params: request parameters
             instances_per_page: number of objects per page
         Return:
             List of objects paginated
     """
     # Prepare paginator
     paginator = Paginator(instance_list, instances_per_page)
-    page = request.GET.get('page', 1)
+    page = params.get('page', 1)
 
     # Paginate queryset
     try:
