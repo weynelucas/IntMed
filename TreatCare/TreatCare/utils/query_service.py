489d674c96a82acb416d047659cd813f59f4e7b4
Lucas Weyne <lucas.weyne@lsbd.ufc.br>
2016-08-24 18:00:20 -0300

    Paginate and slice data

--- TreatCare/TreatCare/utils/query_service.py
+++ TreatCare/TreatCare/utils/query_service.py
@@ -1,14 +1,18 @@
+import operator
+from functools import reduce
 from django.apps import apps
+from django.db.models import Q
 from django.db.models.sql.constants import QUERY_TERMS
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 
-def perform_query(app_label, model_name, params):
+def perform_query(app_label, model_name, params, or_query=False):
     """ Performs a query in a model given the request
         parameters
         Args:
             app_label: app containing the model to perform the query
             model_name: name of model to perform the query
             params: request parameters
+            or_query: if clauses are grouped by OR operands (default is False)
         Return:
             List of objects (queryset)
     """
@@ -19,7 +23,11 @@ def perform_query(app_label, model_name, params):
     order = params.get('order', 'asc')
 
     query_params = {key: value for key, value in params.items() if (key.split('__')[0] in str(fields)) and (key.split('__')[-1] in QUERY_TERMS)}
-    queryset = Model.objects.filter(**query_params)
+    if or_query:
+        list_of_Q = [Q(**{key: val}) for key, val in query_params.items()]
+        queryset = Model.objects.filter(reduce(operator.or_, list_of_Q))
+    else:
+        queryset = Model.objects.filter(**query_params)
 
     if sort:
         order_by = ''
@@ -30,6 +38,22 @@ def perform_query(app_label, model_name, params):
 
     return queryset
 
+def perform_lookup_query(app_label, model_name, params, query):
+    """ Perform a query in a model to lookup the occurrence of
+        the given argument in all model fields
+        Args:
+            app_label: app containing the model to perform the query
+            model_name: name of model to perform the query
+            params: request parameters
+            query: value to lookup
+        Return:
+            List of objects (queryset)
+    """
+    Model = apps.get_model(app_label=app_label, model_name=model_name)
+    fields = [field.name for field in Model._meta.fields]
+    new_params = params.copy()
+    new_params.update({field + '__icontains': query for field in fields})
+    return perform_query(app_label=app_label, model_name=model_name, params=new_params, or_query=True)
 
 def paginate_list(instance_list, params, instances_per_page=20):
     """ Paginate a list of objects
--- TreatCare/medicine/views.py
+++ TreatCare/medicine/views.py
@@ -1,13 +1,19 @@
 from django.shortcuts import render
 from .models import Medicine
 from TreatCare.utils import query_service
-
+import math
 # Create your views here.
 def list(request):
     params = request.GET.copy()
-    objects = query_service.perform_query(app_label='medicine', model_name='Medicine', params=params)
-    objects = query_service.paginate_list(objects, params, 50)
+    q = params.get('q', '')
+    items_per_page = params.get('items_per_page', 50)
+    objects = query_service.perform_lookup_query(app_label='medicine', model_name='Medicine', params=params, query=q)
+    objects =  query_service.paginate_list(objects, params, items_per_page)
     context = {
         'objects': objects,
+        'objects_total': objects.paginator.count,
+        'query': params,
+        'items_per_page': items_per_page,
+        'page': objects.paginator.page(params.get('page', 1)),
     }
     return render(request, 'medicine/list.html', context)
