5bb73fb2ebbc615a30a4117c57664350935ad3c3
Lucas Weyne <weynelucas@gmail.com>
2016-08-25 08:23:11 -0300

    Fix &amp on URL

--- TreatCare/medicine/views.py
+++ TreatCare/medicine/views.py
@@ -6,14 +6,16 @@ import math
 def list(request):
     params = request.GET.copy()
     q = params.get('q', '')
-    items_per_page = params.get('items_per_page', 50)
+    items_per_page = params.get('items_per_page', '50')
     objects = query_service.perform_lookup_query(app_label='medicine', model_name='Medicine', params=params, query=q)
-    objects =  query_service.paginate_list(objects, params, items_per_page)
+    objects = query_service.paginate_list(objects, params, items_per_page)
+
     context = {
         'objects': objects,
         'objects_total': objects.paginator.count,
         'query': params,
         'items_per_page': items_per_page,
-        'page': objects.paginator.page(params.get('page', 1)),
+        'start_index': objects.start_index,
+        'end_index': objects.end_index,
     }
     return render(request, 'medicine/list.html', context)
