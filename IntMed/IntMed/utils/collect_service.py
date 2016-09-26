bf81112ea21af55e3f90336f265ce31d20d77c7b
Lucas Weyne <weynelucas@gmail.com>
2016-09-25 21:54:43 -0300

    Requirements

--- IntMed/IntMed/utils/collect_service.py
+++ IntMed/IntMed/utils/collect_service.py
@@ -1,5 +1,4 @@
 import http.client as http
-from bs4 import BeautifulSoup
 from .database_service import db_table_exists
 from interactions.neo4j_interface import connection
 
@@ -20,18 +19,3 @@ def load_drugs(DrugClass=None):
          for record in result:
              drug = DrugClass(id = record['id'], name=record['name'], action=record['action'])
              drug.save()
-
-
-def get_html_soup_response(url, uri, port=80):
-    """ Get the html of a HTTP response parsed by
-        BeautifulSoup
-        Args:
-            url: URL of HTTP connection
-            uri: URI of HTTP connection
-            port: port number of HTPP connection
-    """
-    conn = http.HTTPConnection(url, 80)
-    conn.request("GET", uri)
-    resp = conn.getresponse()
-
-    return BeautifulSoup(resp.read(), 'html.parser')
